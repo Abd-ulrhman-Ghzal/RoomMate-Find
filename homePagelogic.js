6867b3c2a6bc0071b8d2842f061588216b6e03db
Abd-ulrhman-Ghzal <129174464+Abd-ulrhman-Ghzal@users.noreply.github.com>
2024-08-29 22:51:47 +0300

    ads

--- homePagelogic.js
+++ homePagelogic.js
@@ -1,8 +1,12 @@
 const buttonRight = document.getElementById('slideRight');
 const buttonLeft = document.getElementById('slideLeft');
+const notificationBTN=document.getElementById('notification-btn')
+let notificationIMG=document.getElementById('notification-img')
 let homeContent=document.getElementById('home-containt')
 let chatContent=document.getElementById('Chat-content')
 let nav=document.getElementById('header')
+let notificationContent=document.getElementById('notification-content')
+
 
 const myCarouselElement = document.querySelector('#carouselExampleSlidesOnly')
 
@@ -31,9 +35,53 @@ chatbtn.onclick =function(){
 
 function showchat(){
   homeContent.style.display='none';
+    chatContent.style.display='block';
+    notificationContent.style.display='none';
+    adSeekerpage.style.display='none'
   chatimg.src='./images/home page/chat active.png'
-  chatbtn.style.pointerEvents='d'
-  chatContent.style.display='block'
   nav.style.cssText='position: relative;margin: 0;'
   document.querySelector('body').style.backgroundColor='#f2eaccc6'
+  notificationIMG.src='./images/bell-fill.png'
+}
+
+function notificationDiv(){
+  homeContent.style.display='none';
+    chatContent.style.display='none';
+    notificationContent.style.display='block';
+    adSeekerpage.style.display='none'
+  notificationIMG.src='./images/bell-active.png'
+  chatimg.src='./images/home page/chat.png'
+  nav.style.cssText='position: relative;margin: 0;'
+  document.querySelector('body').style.backgroundImage='linear-gradient(180deg, #F8F4DB, white)'
+}
+
+// card select logic
+
+let cardsCon=document.querySelectorAll('.appartment-card')
+let adSeekerpage=document.getElementById('Ad-seeker')
+for(let i=0;i<cardsCon.length;i++){
+  cardsCon[i].onclick = function(){
+    nav.style.cssText='position: relative;margin: 0;'
+    homeContent.style.display='none';
+    chatContent.style.display='none';
+    notificationContent.style.display='none';
+    adSeekerpage.style.display='block'
+    document.querySelector('body').style.backgroundImage='linear-gradient(180deg, #F8F4DB, white)'
+    
+  }
+}
+
+let roomateFinder=document.getElementById('seeker-roomate')
+let personCard=document.querySelectorAll('.person-card')
+
+for(let i=0;i<personCard.length;i++){
+  personCard[i].onclick = function(){
+    nav.style.cssText='position: relative;margin: 0;'
+    homeContent.style.display='none';
+    chatContent.style.display='none';
+    notificationContent.style.display='none';
+    adSeekerpage.style.display='none'
+    roomateFinder.style.display='block'
+    document.querySelector('body').style.backgroundImage='linear-gradient(180deg, #F8F4DB, white)'
+  }
 }
